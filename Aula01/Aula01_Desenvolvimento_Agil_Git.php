bab9821d9811fd25c485223e66c2f6dd67e5594b
matheusmsantana <matheus.marques.santana@hotmail.com>
2018-02-06 02:29:07 -0200

    Final da Aula01

---
+++ Aula01/Aula01_Desenvolvimento_Agil_Git.php
@@ -0,0 +1,17 @@
+<?php
+
+    /**
+     * Boas praticas de programação 
+     * 
+     * 
+     * Keep It  Simple Stupid (KISS): Mantenha seu código simples para e de facil compreensão.
+     * 
+     * Tell, Don't Ask: Um codigo de dificil compreenção esta errado.
+     * 
+     * You Aren't Gonna Need It(YAGIN): Codifique apenas oque vai ser Utilizado.
+     * 
+     * Separation Of Concerns(SoC): Divida seu código em blocos, para que seja facil dar manutenção no mesmo,
+     * e para que seja reaproveiado em outros blocos de código.
+     */
+
+?>
